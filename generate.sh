7d6456feb9712bbe31b11f600a7b58fe3b395d39
Van Waholtz <vanwaholtz@gmail.com>
2020-03-02 14:32:21 +0800

    Use the OpenWrt 19.07.2.
    Signed-off-by: Van Waholtz <vanwaholtz@gmail.com>

--- generate.sh
+++ generate.sh
@@ -10,7 +10,23 @@ sed -i 's/git\.openwrt\.org\/feed\/telephony/github\.com\/openwrt\/telephony/g' 
 ./scripts/feeds update -a
 ./scripts/feeds install -a
 
-make defconfig V=s -j1
+make defconfig V=s
 
 sed -i "s/.*CONFIG_qbt_daemon_Lang-zh[=\| ].*/CONFIG_qbt_daemon_Lang-zh=y/g" .config
 sed -i "s/.*CONFIG_qbt_webui_Lang-zh[=\| ].*/CONFIG_qbt_webui_Lang-zh=y/g" .config
+
+make package/luci-app-qbittorrent/compile V=s -j$(nproc)
+
+export TARGET_PATH=build/bin/targets/${{env.USE_TARGET}}/${{env.USE_SUBTARGET}}
+export PACKAGE_PATH=build/bin/packages/${{env.USE_ARCH}}
+
+mkdir -p ./${{env.USE_ARCH}}/1 ./${{env.USE_ARCH}}/2
+cp -f ${PACKAGE_PATH}/base/qt5* ./${{env.USE_ARCH}}/1
+cp -f ${PACKAGE_PATH}/base/*torrent*.ipk ./${{env.USE_ARCH}}/1
+cp -f ${PACKAGE_PATH}/base/libopenssl* ./${{env.USE_ARCH}}/2
+cp -f ${PACKAGE_PATH}/packages/boost-system* ./${{env.USE_ARCH}}/2
+cp -f ${PACKAGE_PATH}/packages/libdouble-conversion* ./${{env.USE_ARCH}}/2
+cp -f ${PACKAGE_PATH}/packages/libpcre2-16* ./${{env.USE_ARCH}}/2
+cp -f ${PACKAGE_PATH}/base/zlib_* ./${{env.USE_ARCH}}/2
+cp -f ${TARGET_PATH}/packages/libstdcpp* ./${{env.USE_ARCH}}/2
+tar -cJvf ${{env.USE_ARCH}}.tar.xz ${{env.USE_ARCH}}
