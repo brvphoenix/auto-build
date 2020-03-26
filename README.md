3ae6b74d3358ba6e50a53a6f07a8ee03a0fc32ea
Van Waholtz <vanwaholtz@gmail.com>
2020-03-26 10:28:42 +0800

    Release v4.2.2.
    Signed-off-by: Van Waholtz <vanwaholtz@gmail.com>

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 ![check](https://github.com/brvphoenix/auto-build/workflows/CI/badge.svg)
 [![Lastest Release](https://img.shields.io/github/release/brvphoenix/auto-build.svg?style=flat)](https://github.com/brvphoenix/auto-build/releases)
 
-Current version 4.2.1.
+Current version 4.2.2.
 
 
--- generate.sh
+++ generate.sh
@@ -3,6 +3,9 @@ USE_TARGET=$1
 USE_SUBTARGET=$2
 USE_ARCH=$3
 
+rm -rf build/package/qbittorrent/libtorrent-rasterbar
+mv ./libtorrent-rasterbar build/package/qbittorrent/
+
 cd build
 
 sed -i 's/git\.openwrt\.org\/openwrt\/openwrt/github\.com\/openwrt\/openwrt/g' ./feeds.conf.default
@@ -13,24 +16,29 @@ sed -i 's/git\.openwrt\.org\/feed\/telephony/github\.com\/openwrt\/telephony/g' 
 ./scripts/feeds update -a
 ./scripts/feeds install -a
 
-make defconfig V=s
+make defconfig
 
-sed -i "s/.*CONFIG_qbt_daemon_Lang-zh[=\| ].*/CONFIG_qbt_daemon_Lang-zh=y/g" .config
-sed -i "s/.*CONFIG_qbt_webui_Lang-zh[=\| ].*/CONFIG_qbt_webui_Lang-zh=y/g" .config
+sed -i "s/.*\(CONFIG_QT5_STATIC_BUILD\).*/\1=y/g" .config
+sed -i "s/.*\(CONFIG_QT5_USING_SYSTEM_DC\).*/# \1 is not set/g" .config
+sed -i "s/.*\(CONFIG_QT5_USING_SYSTEM_PCRE2\).*/# \1 is not set/g" .config
+sed -i "s/.*\(CONFIG_QT5_USING_SYSTEM_ZLIB\).*/# \1 is not set/g" .config
+
+sed -i "s/.*\(CONFIG_QBT_LIBTORRENT_STATIC_LINK\).*/\1=y/g" .config
+sed -i "s/.*\(CONFIG_QBT_ZLIB_STATIC_LINK\).*/\1=y/g" .config
+
+sed -i "s/.*CONFIG_QBT_DAEMON_LANG-zh[=\| ].*/CONFIG_QBT_DAEMON_LANG-zh=y/g" .config
+sed -i "s/.*CONFIG_QBT_WEBUI_LANG-zh[=\| ].*/CONFIG_QBT_WEBUI_LANG-zh=y/g" .config
+
+make defconfig
+
+sed -i "s/.*\(CONFIG_QT5_OPENSSL_STATIC_RUNTIME\).*/\1=y/g" .config
+sed -i "s/.*\(CONFIG_LUCI_LANG_zh_Hans\).*/\1=y/g" .config
 
 make package/luci-app-qbittorrent/compile V=s -j$(nproc)
 
-export TARGET_PATH=build/bin/targets/${USE_TARGET}/${USE_SUBTARGET}
 export PACKAGE_PATH=build/bin/packages/${USE_ARCH}
 
 cd ..
-mkdir -p ./${USE_ARCH}/1 ./${USE_ARCH}/2
-cp -f ${PACKAGE_PATH}/base/qt5* ./${USE_ARCH}/1
-cp -f ${PACKAGE_PATH}/base/*torrent*.ipk ./${USE_ARCH}/1
-cp -f ${PACKAGE_PATH}/base/libopenssl* ./${USE_ARCH}/2
-cp -f ${PACKAGE_PATH}/packages/boost-system* ./${USE_ARCH}/2
-cp -f ${PACKAGE_PATH}/packages/libdouble-conversion* ./${USE_ARCH}/2
-cp -f ${PACKAGE_PATH}/packages/libpcre2-16* ./${USE_ARCH}/2
-cp -f ${PACKAGE_PATH}/base/zlib_* ./${USE_ARCH}/2
-cp -f ${TARGET_PATH}/packages/libstdcpp* ./${USE_ARCH}/2
-tar -cJvf ${USE_ARCH}.tar.xz ${USE_ARCH}
+mkdir -p ./${USE_ARCH}-static
+cp -f ${PACKAGE_PATH}/base/*qbittorrent*.ipk ./${USE_ARCH}-static/
+tar -cJvf ${USE_ARCH}-static.tar.xz ${USE_ARCH}-static
