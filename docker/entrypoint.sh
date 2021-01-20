b60c8a3a86ba53f73ca0eb6480f515e40271a458
brvphoenix <vanwaholtz@gmail.com>
2021-01-19 17:42:13 -0800

    Release 4.3.3

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,7 +3,7 @@
 mkdir -p /var/lock/
 
 opkg update
-opkg install $(find /ci -type f -iname '*.ipk')
+opkg print-architecture | awk -F ' ' '{print "--add-arch " $2 ":" $3}' | xargs opkg --add-arch $1:100 install $(find /ci -type f -iname '*.ipk')
 
 cat /etc/banner
 echo "-------------------------------------------"
