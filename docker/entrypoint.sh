fd338cba4a1cdf34df13d17d31f82aeed7520eb7
brvphoenix <vanwaholtz@gmail.com>
2022-01-24 10:05:27 +0800

    Don't don't include the sdk information in workflow file

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -11,6 +11,8 @@ opkg print-architecture
 echo "-------------------------------------------"
 
 echo "Architecture: $(uname -m)"
+# Old arm devices need /lib/ld-musl-arm.so.1
+[ -f "/lib/ld-musl-armhf.so.1" ] && ln -sf '/lib/ld-musl-armhf.so.1' '/lib/ld-musl-arm.so.1'
 qbittorrent-nox -v
 
 qbittorrent-nox --profile=/tmp 2>&1 &
