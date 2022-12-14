brvphoenix/auto-build

commit 2f3cc7af489f5e32b1f709a66552c835844a5a40

Release 4.5.0-1

.github/workflows/ci.yml
@@ -299,6 +299,8 @@ jobs:
         curl -s http://127.0.0.1:28181/api/v2/app/webapiVersion --cookie "SID=${sid}" | xargs -i echo "WebAPI: {}"
         echo "-------------------------------------------"
         curl -s http://127.0.0.1:28181/api/v2/app/buildInfo --cookie "SID=${sid}" | jq -r 'to_entries[] | "\(.key): \(.value)"'
+        echo "-------------------------------------------"
+        curl -s http://127.0.0.1:28181/api/v2/app/supportedLanguages --cookie "SID=${sid}" | jq -r 'to_entries[] | "\(.key): \(.value)"'
         curl -s -X POST http://127.0.0.1:28181/api/v2/app/shutdown --cookie "SID=${sid}"
         echo "::endgroup::"
   pre-release:

build_pre.sh
@@ -38,6 +38,10 @@ if [ "${qt_ver}" = "5" ]; then
 fi
 
 # Pathes has not been contained in the upstream.
-curl -kLOZ --compressed ${GITHUB_SERVER_URL}/${GITHUB_REPOSITORY_OWNER}/qBittorrent/commit/daaf8a6f5.patch
-mkdir -p ./mirror/package/self/qbittorrent/patches
-mv daaf8a6f5.patch ./mirror/package/self/qbittorrent/patches/0001-daaf8a6f5.patch
+mkdir -p ../mirror/package/self/qbittorrent/patches
+
+curl -kLOZ --compressed https://patch-diff.githubusercontent.com/raw/qbittorrent/qBittorrent/pull/18076.patch
+mv 18076.patch ../mirror/package/self/qbittorrent/patches/0001-18076.patch
+
+curl -kLOZ --compressed https://patch-diff.githubusercontent.com/raw/qbittorrent/qBittorrent/pull/18083.patch
+mv 18083.patch ../mirror/package/self/qbittorrent/patches/0002-18083.patch

build_conf.sh
@@ -50,7 +50,7 @@ cat > .config <<EOF
 # CONFIG_ALL is not set
 CONFIG_PACKAGE_luci-app-qbittorrent=y
 CONFIG_QBT_REMOVE_GUI_TR=y
-CONFIG_QBT_LANG-zh=y
+CONFIG_QBT_LANG-zh_CN=y
 CONFIG_LUCI_LANG_zh_Hans=y
 EOF
 

commit 0615af0abaa97b41f095c5a470e50b77dbd26fb9

Release v4.5.0-2
Fix a regression from 2f3cc7af489f5e32b1f709a66552c835844a5a40.

.github/workflows/ci.yml
@@ -299,8 +299,6 @@ jobs:
         curl -s http://127.0.0.1:28181/api/v2/app/webapiVersion --cookie "SID=${sid}" | xargs -i echo "WebAPI: {}"
         echo "-------------------------------------------"
         curl -s http://127.0.0.1:28181/api/v2/app/buildInfo --cookie "SID=${sid}" | jq -r 'to_entries[] | "\(.key): \(.value)"'
-        echo "-------------------------------------------"
-        curl -s http://127.0.0.1:28181/api/v2/app/supportedLanguages --cookie "SID=${sid}" | jq -r 'to_entries[] | "\(.key): \(.value)"'
         curl -s -X POST http://127.0.0.1:28181/api/v2/app/shutdown --cookie "SID=${sid}"
         echo "::endgroup::"
   pre-release:

build_pre.sh
@@ -38,10 +38,15 @@ if [ "${qt_ver}" = "5" ]; then
 fi
 
 # Pathes has not been contained in the upstream.
-mkdir -p ../mirror/package/self/qbittorrent/patches
+mkdir -p ./mirror/package/self/qbittorrent/patches
 
-curl -kLOZ --compressed https://patch-diff.githubusercontent.com/raw/qbittorrent/qBittorrent/pull/18076.patch
-mv 18076.patch ../mirror/package/self/qbittorrent/patches/0001-18076.patch
+curl -kLOZ --compressed https://patch-diff.githubusercontent.com/raw/qbittorrent/qBittorrent/pull/18231.patch
+mv 18231.patch ./mirror/package/self/qbittorrent/patches/0000-18231.patch
 
-curl -kLOZ --compressed https://patch-diff.githubusercontent.com/raw/qbittorrent/qBittorrent/pull/18083.patch
-mv 18083.patch ../mirror/package/self/qbittorrent/patches/0002-18083.patch
+# Hotfixes for official v4_5_x
+curl -kLZ --compressed -o 0001.patch https://github.com/qbittorrent/qBittorrent/compare/release-4.5.0...v4_5_x.patch
+mv 0001.patch ./mirror/package/self/qbittorrent/patches/0001.patch
+
+# Log view
+curl -kLZ --compressed -o 0002.patch https://github.com/brvphoenix/qBittorrent/compare/84fabf14c8667d153375d1041fe508dfc66d0fee...logs.patch
+mv 0002.patch ./mirror/package/self/qbittorrent/patches/0002.patch