d94b9259b0440327ba3ab8fdfd864193422b788e
brvphoenix <30111323+brvphoenix@users.noreply.github.com>
2023-05-07 19:11:59 +0800

    Add support for self-built rootfs
    1. Target mvebu-cortexa9 has its own docker since 22.03.0
    2. Add QEMU_CPU environment variable to tune the instruction sets.

--- docker/rootfs/entrypoint.sh
+++ docker/rootfs/entrypoint.sh
@@ -12,8 +12,5 @@ echo ""
 echo "Architecture: $(uname -m)"
 echo "-------------------------------------------"
 
-# Old arm devices need /lib/ld-musl-arm.so.1
-[ -f "/lib/ld-musl-armhf.so.1" ] && ln -sf '/lib/ld-musl-armhf.so.1' '/lib/ld-musl-arm.so.1'
-
 mkdir -p /tmp/qbittorrent
 qbittorrent-nox --profile=/tmp/qbittorrent
--- init_env.sh
+++ init_env.sh
@@ -19,19 +19,27 @@ for option in $(jq -r '.openwrt."'${target_arch}'" | to_entries[] | "\(.key)=\(.
 	eval "${option}"
 done
 
-sdkfile="openwrt-sdk-.*.Linux-x86_64.tar.xz"
-
 [ "${USE_VERSION}" = "snapshots" ] && version_path="snapshots" || version_path="releases/${USE_VERSION}"
 USE_SOURCE_URL=${USE_DOWNLOAD_SERVER}/${version_path}/targets/${USE_TARGET//-/\/}
 
 curl -ksLOZ --compressed ${USE_SOURCE_URL}/sha256sums
-USE_SDK_FILE=$(grep -io "${sdkfile}" sha256sums)
-USE_SDK_SHA256SUM=$(grep -i "${sdkfile}" sha256sums | cut -d' ' -f1)
-[ -n "${USE_SDK_FILE}" ] || exit 1;
+sdkfile="openwrt-sdk-.*.Linux-x86_64.tar.xz"
+grep -i "${sdkfile}" sha256sums > sdk.sha256sums
+USE_SDK_FILE=$(grep -io "${sdkfile}" sdk.sha256sums)
 
 echo "USE_SDK_FILE=${USE_SDK_FILE}" >> $GITHUB_ENV
-echo "USE_SDK_SHA256SUM=${USE_SDK_SHA256SUM}" >> $GITHUB_ENV
 echo "USE_SOURCE_URL=${USE_SOURCE_URL}" >> $GITHUB_ENV
+[ -n "${USE_SDK_FILE}" ] || exit 1;
+
+if [ "${USE_IMAGEBUILDER}" = 'true' -a "${RUNTIME_TEST}" = "true" ]; then
+	imagebuilderfile="openwrt-imagebuilder-.*.Linux-x86_64.tar.xz"
+	grep -i "${imagebuilderfile}" sha256sums > imagebuilder.sha256sums
+	USE_IMAGEBUILDER_FILE=$(grep -io "${imagebuilderfile}" imagebuilder.sha256sums)
+
+	echo "USE_IMAGEBUILDER_FILE=${USE_IMAGEBUILDER_FILE}" >> $GITHUB_ENV
+	echo "USE_IMAGEBUILDER=${USE_IMAGEBUILDER}" >> $GITHUB_ENV
+	[ -n "${USE_IMAGEBUILDER_FILE}" ] || exit 1
+fi
 
 # QBT source and libtorrent source info
 echo USE_QBT_REFS=$(jq -r '.qbittorrent.QT_VERSION?."'${qt_ver}'"' ${JSON_FILE}) >> $GITHUB_ENV
@@ -52,7 +60,7 @@ http_code=$(curl -fskILZ -o /dev/null -w %{http_code} --compressed ${USE_SOURCE_
 	sdk_ver="$(curl -skLZ --compressed ${USE_SOURCE_URL}/version.buildinfo)" || \
 	sdk_ver="${GITHUB_RUN_ID}"
 
-echo "USE_SDK_VERSION=${sdk_ver}" >> $GITHUB_ENV
+echo "USE_OPENWRT_SRC_VERSION=${sdk_ver}" >> $GITHUB_ENV
 
 case "${USE_VERSION}" in
 *-SNAPSHOT)
@@ -85,11 +93,15 @@ echo "USE_DOCKER_ROOTFS_TAG=${docker_rootfs_tag}" >> $GITHUB_ENV
 
 # Get the docker image hash
 if [ "${RUNTIME_TEST}" = "true" ]; then
-	# openwrt/rootfs
-	token=$(curl -skL "https://ghcr.io/token?scope=repository:openwrt/rootfs:pull&service=ghcr.io" | jq -r '.token')
-	curl -fskILZ -H "Accept: application/vnd.docker.distribution.manifest.v2+json" \
-		-H "Authorization: Bearer ${token}" "https://ghcr.io/v2/openwrt/rootfs/manifests/${docker_rootfs_tag}" \
-		| sed -n 's/docker-content-digest:\s\+sha256:\(\w\+\)/\1/gp' | xargs -i echo "USE_ROOTFS_HASH={}" >> $GITHUB_ENV || exit 1
+	[ -n "${USE_QEMU_CPU}" ] && echo "QEMU_CPU=${USE_QEMU_CPU}" >> ${GITHUB_WORKSPACE}/docker_env || touch ${GITHUB_WORKSPACE}/docker_env
+
+	if [ "${USE_IMAGEBUILDER}" != 'true' ]; then
+		# openwrt/rootfs
+		token=$(curl -skL "https://ghcr.io/token?scope=repository:openwrt/rootfs:pull&service=ghcr.io" | jq -r '.token')
+		curl -fskILZ -H "Accept: application/vnd.docker.distribution.manifest.v2+json" \
+			-H "Authorization: Bearer ${token}" "https://ghcr.io/v2/openwrt/rootfs/manifests/${docker_rootfs_tag}" \
+			| sed -n 's/docker-content-digest:\s\+sha256:\(\w\+\)/\1/gp' | xargs -i echo "USE_ROOTFS_HASH={}" >> $GITHUB_ENV || exit 1
+	fi
 fi
 
 # Common name of the saved files
