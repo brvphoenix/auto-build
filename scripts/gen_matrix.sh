cfb4767d43f5f7cc7b9ce70cf9405326dd95dfaa
brvphoenix <30111323+brvphoenix@users.noreply.github.com>
2023-10-29 11:12:45 +0800

    Minor fixes

--- scripts/gen_matrix.sh
+++ scripts/gen_matrix.sh
@@ -9,7 +9,7 @@ for json_file in ./*.json; do
 	[ -z "${inputs_type}" ] || [ "${inputs_type}" = "all" ] || [ "${inputs_type}" = "${link}" ] || continue;
 
 	matrix=$(jq -c --argjson mt "${matrix}" --arg lt "${inputs_lt/./_}" --arg qt "${inputs_qt}" --arg target "${inputs_target}" --arg link "${link}" '
-		foreach (.openwrt | (if (.RUN_SKIP? // false) then [] else . end) | to_entries | map(select((.value | type) == "object"))) as $item (
+		[foreach (.openwrt | (if (.RUN_SKIP? // false) then [] else . end) | to_entries | .[] | select((.value | type) == "object")) as $item (
 			[{
 				lt: .qbittorrent.LIBTORRENT_VERSION? | to_entries[]? | .key? | select($lt == "" or $lt == "all" or $lt == .),
 				qt: .qbittorrent.QT_VERSION? | to_entries[]? | .key? | select($qt == "" or $qt == "all" or $qt == .),
@@ -18,9 +18,9 @@ for json_file in ./*.json; do
 					select($target == "" or $target == "all" or (split("-") | .[0]) == $target)
 				)
 			}];
-			map(select(.target == $item[].key) | .target as $tg | . += {runtime_test: $item[] | select(.key == $tg) | (.value?.RUNTIME_TEST? // false)});
-			.
-		) | . + $mt | unique | sort_by(.link + .target)
+			map(select(.target == $item.key) += {runtime_test: ($item.value?.RUNTIME_TEST? // false)});
+			.[] | select(.target == $item.key)
+		)] | . + $mt | unique | sort_by(.link + .target)
 	' ${link}.json)
 done
 
