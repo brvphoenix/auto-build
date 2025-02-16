d045d32bff1f6e946e8d4b46bd65bf74c4315379
brvphoenix <30111323+brvphoenix@users.noreply.github.com>
2025-02-16 20:38:48 +0800

    qemu: revert qemu to 7.2
    MIPS cannot run test with qemu 9.2.

--- scripts/register_qemu.sh
+++ scripts/register_qemu.sh
@@ -13,20 +13,20 @@ set -eET -o pipefail
 mkdir -p qemu qemu/qus qemu/tmp
 cd qemu
 
-codename=$(curl -ksfL http://ftp.debian.org/debian/dists/testing/InRelease | grep 'Codename:' | cut -d' ' -f2)
-qus_ver=$(curl -ksfL https://sources.debian.org/api/src/qemu | jq -r --arg codename "$codename" '.versions | map(select(.suites[] | contains($codename))) | .[0].version | split(":") | .[-1]')
+codename=$(curl -ksfL http://ftp.debian.org/debian/dists/bookworm/InRelease | grep 'Codename:' | cut -d' ' -f2)
+qus_ver=$(curl -ksfL https://sources.debian.org/api/src/qemu | jq -r --arg codename "$codename" '.versions | map(select(.suites[] | . == $codename)) | .[0].version | split(":") | .[-1]')
 
-curl -fkLOZ --compressed --connect-timeout 10 --retry 5 http://ftp.debian.org/debian/pool/main/q/qemu/qemu-user_${qus_ver}_amd64.deb
-dpkg -x "qemu-user_${qus_ver}_amd64.deb" "$(pwd)/qus"
+curl -fkLOZ --verbose --compressed --connect-timeout 10 --retry 5 http://ftp.debian.org/debian/pool/main/q/qemu/qemu-user-static_${qus_ver}_amd64.deb
+dpkg -x "qemu-user-static_${qus_ver}_amd64.deb" "$(pwd)/qus"
 
 # Register qemu by official binfmt.
 exportdir=$(pwd)/tmp
-binfmt_ver=$(echo ${qus_ver} | sed -n 's,\([0-9]\+\.[0-9]\+\).*,\1,gp')
+binfmt_ver=$(echo 9.1 | sed -n 's,\([0-9]\+\.[0-9]\+\).*,\1,gp')
 curl -fkLOZ --compressed --connect-timeout 10 --retry 5 https://raw.githubusercontent.com/qemu/qemu/refs/heads/stable-${binfmt_ver}/scripts/qemu-binfmt-conf.sh
 chmod +x qemu-binfmt-conf.sh
 
 # Modify the package name to avoid potential conflits. For example, it need the modify the package name
 # to 'qemu-user-static' if use package 'qemu-user-static'.
 sed -i 's/^package qemu-\$cpu$/package qemu-test-static/g' qemu-binfmt-conf.sh
-./qemu-binfmt-conf.sh --qemu-path "$(pwd)/qus/usr/bin" --debian --exportdir "${exportdir}" --persistent yes
+./qemu-binfmt-conf.sh --qemu-suffix "-static" --qemu-path "$(pwd)/qus/usr/bin" --debian --exportdir "${exportdir}" --persistent yes
 sudo update-binfmts --importdir ${exportdir} --import
