bad211df19e034b9582789bbb0ad01909df9ab28
brvphoenix <30111323+brvphoenix@users.noreply.github.com>
2025-05-17 17:23:40 +0800

    Move to snapshot branch for static build

--- scripts/pack_all.sh
+++ scripts/pack_all.sh
@@ -5,11 +5,20 @@ set -eET -o pipefail
 
 [ -d "${CUR_SDK_DIR_NAME}" ] && cd ${CUR_SDK_DIR_NAME} || { echo "Not exist '${CUR_SDK_DIR_NAME}' directory"; exit 0; }
 
-target_arch=$(perl ./scripts/dump-target-info.pl targets 2>/dev/null | grep "^${CUR_TARGET_NAME//-/\/}\b" | cut -d ' ' -f 2)
+TOPDIR=$(pwd)
+STAGING_DIR_HOST=${TOPDIR}/staging_dir/host
+SCRIPT_DIR=${TOPDIR}/scripts
 
 SAVE_ROOT_DIR=${GITHUB_WORKSPACE}/qbittorrent_${CUR_TARGET_NAME}
 PKGS_DIR=${SAVE_ROOT_DIR}/pkgs
 KEY_DIR=${SAVE_ROOT_DIR}/key
+
+PKG_EXT=$([ "${CUR_LINK_TYPE}" = "static" ] && echo apk || echo ipk)
+PKG_ARCH=$(perl ${SCRIPT_DIR}/dump-target-info.pl targets 2>/dev/null | grep "^${CUR_TARGET_NAME//-/\/}\b" | cut -d ' ' -f 2)
+
+BUILD_KEY_OPKG_NAME=qbt-key
+BUILD_KEY_APK_SEC=${TOPDIR}/qbt-private-key.pem
+BUILD_KEY_APK_PUB=${TOPDIR}/qbt-public-key.pem
 
 if [ "${CACHE_HIT}" = "true" ]; then
 	fingerprint=$(ls -t ${KEY_DIR} | head -n 1)
@@ -19,110 +28,165 @@ else
 		[ ! -d "./bin/packages" ] || find ./bin/packages -type f -iname *qbittorrent* -exec cp -f {} ${PKGS_DIR} \;
 	else
 		[ "$CUR_LIBT_VERSION" = "1_2" ] && {
-			[ ! -d "./bin/packages" ] || find ./bin/packages -type f -iname *.ipk -exec cp -f {} ${PKGS_DIR} \;
+			[ ! -d "./bin/packages" ] || find ./bin/packages -type f -iname *.${PKG_EXT} -exec cp -f {} ${PKGS_DIR} \;
 			[ ! -d "./bin/targets" ] || find ./bin/targets -type f \( \
-				-iname libstdcpp*.ipk -o \
-				-iname libatomic*.ipk -o \
-				-iname librt*.ipk \
+				-iname libstdcpp*.${PKG_EXT} -o \
+				-iname libatomic*.${PKG_EXT} -o \
+				-iname librt*.${PKG_EXT} \
 			\) -exec cp -f {} ${PKGS_DIR} \;
 		} || {
-			[ ! -d "./bin/packages" ] || find ./bin/packages -type f -iname *.ipk ! -iname boost_*.ipk ! -iname boost-*.ipk -exec cp -f {} ${PKGS_DIR} \;
+			[ ! -d "./bin/packages" ] || find ./bin/packages -type f -iname *.${PKG_EXT} ! -iname boost_*.${PKG_EXT} ! -iname boost-*.${PKG_EXT} -exec cp -f {} ${PKGS_DIR} \;
 			[ ! -d "./bin/targets" ] || find ./bin/targets -type f \( \
-				-iname libstdcpp*.ipk -o \
-				-iname libatomic*.ipk \
+				-iname libstdcpp*.${PKG_EXT} -o \
+				-iname libatomic*.${PKG_EXT} \
 			\) -exec cp -f {} ${PKGS_DIR} \;
 		}
 	fi
 
-	STAGING_DIR_HOST=$(pwd)/staging_dir/host
-	SCRIPT_DIR=$(pwd)/scripts
-	BUILD_KEY=qbt-key
-	export MKHASH=${STAGING_DIR_HOST}/bin/mkhash
-	export PATH=${STAGING_DIR_HOST}/bin:$PATH
+	# Packages index
+	if [ "${CUR_LINK_TYPE}" = "static" ]; then
+		# Index the packages
+		cd ${PKGS_DIR}
+		${STAGING_DIR_HOST}/bin/openssl ecparam -name prime256v1 -genkey -noout -out ${BUILD_KEY_APK_SEC}
+		${STAGING_DIR_HOST}/bin/openssl ec -in ${BUILD_KEY_APK_SEC} -pubout > ${BUILD_KEY_APK_PUB}
 
-	# Index the packages
-	cd ${PKGS_DIR}
-	${SCRIPT_DIR}/ipkg-make-index.sh . 2>&1 > Packages.manifest
-	grep -vE '^(Maintainer|LicenseFiles|Source|SourceName|Require|SourceDateEpoch)' Packages.manifest > Packages
-	case "$$(((64 + $$(stat -L -c%s Packages)) % 128))" in
-	110|111)
-		echo "::warning:: Applying padding in ${PKGS_DIR}/Packages to workaround usign SHA-512 bug!"
-		{ echo ""; echo ""; } >> Packages
-		;;
-	esac
-	gzip -9nc Packages > Packages.gz
-	cd -
+		${STAGING_DIR_HOST}/bin/apk mkndx \
+			--allow-untrusted \
+			--root ${TOPDIR} \
+			--keys-dir ${TOPDIR} \
+			--sign ${BUILD_KEY_APK_SEC} \
+			--output packages.adb \
+			*.apk
+		cp ${BUILD_KEY_APK_PUB} "${KEY_DIR}"
+		cd -
+		fingerprint=${BUILD_KEY_APK_PUB##*/}
+	else
+		export MKHASH=${STAGING_DIR_HOST}/bin/mkhash
+		export PATH=${STAGING_DIR_HOST}/bin:$PATH
 
-	# Sign the packages
-	usign -G -s ${BUILD_KEY} -p ${BUILD_KEY}.pub -c "Local qbt build key"
-	usign -S -m "${PKGS_DIR}/Packages" -s "${BUILD_KEY}"
+		# Index the packages
+		cd ${PKGS_DIR}
+		${SCRIPT_DIR}/ipkg-make-index.sh . 2>&1 > Packages.manifest
+		grep -vE '^(Maintainer|LicenseFiles|Source|SourceName|Require|SourceDateEpoch)' Packages.manifest > Packages
+		case "$$(((64 + $$(stat -L -c%s Packages)) % 128))" in
+		110|111)
+			echo "::warning:: Applying padding in ${PKGS_DIR}/Packages to workaround usign SHA-512 bug!"
+			{ echo ""; echo ""; } >> Packages
+			;;
+		esac
+		gzip -9nc Packages > Packages.gz
+		cd -
 
-	fingerprint=$(usign -F -p ${BUILD_KEY}.pub)
-	cp ${BUILD_KEY}.pub "${KEY_DIR}/$fingerprint"
+		# Sign the packages
+		usign -G -s ${BUILD_KEY_OPKG_NAME} -p ${BUILD_KEY_OPKG_NAME}.pub -c "Local qbt build key"
+		usign -S -m "${PKGS_DIR}/Packages" -s "${BUILD_KEY_OPKG_NAME}"
+
+		fingerprint=$(usign -F -p ${BUILD_KEY_OPKG_NAME}.pub)
+		cp ${BUILD_KEY_OPKG_NAME}.pub "${KEY_DIR}/$fingerprint"
+	fi
 fi
 
 # Generate the install script
-sed 's/^    //g' > ${SAVE_ROOT_DIR}/install.sh <<-"EOF"
-    #!/bin/sh
-    work_dir=$(pwd)
-    script_dir="$(cd "$( dirname "$0" )" && pwd)"
+if [ "${CUR_LINK_TYPE}" = "static" ]; then
+	sed 's/^    //g' > ${SAVE_ROOT_DIR}/install.sh <<-"EOF"
+	    #!/bin/sh
+	    work_dir=$(pwd)
+	    script_dir="$(cd "$( dirname "$0" )" && pwd)"
 
-    cd ${work_dir}
+	    cd ${work_dir}
 
-    if [ "$(opkg print-architecture | sed -n 's/arch \(\S\+\) 10/\1/pg')" != "${target_arch}" ]; then
-    	add_arch=1
-    	cat >> /etc/opkg.conf <<-EOF1
-    		# qbt add start
-    		$(opkg print-architecture)
-    		arch ${target_arch} 1
-    		# qbt add end"
-    	EOF1
-    fi
+	    case "$1" in
+	    	install)
+	    		shift
+	    		cp ${script_dir}/key/$fingerprint /etc/apk/keys
+	    		mkdir -p /tmp/.qbt/apk/repositories.d
+	    		echo "${script_dir}/pkgs/packages.adb" > /tmp/.qbt/apk/repositories.d/customfeeds.list
+	    		cat /tmp/.qbt/apk/repositories.d/customfeeds.list
 
-    case "$1" in
-    	install)
-    		shift
-    		cp ${script_dir}/key/$fingerprint /etc/opkg/keys
-    		sed -i "\$asrc\/gz openwrt_qbt file:\/\/$(echo ${script_dir}/pkgs | sed 's/\//\\\//g')" /etc/opkg/customfeeds.conf
+	    		pkg_arch=$(apk --print-arch)
 
-    		echo "-------------------------------------------"
-    		opkg print-architecture
-    		echo "-------------------------------------------"
+	    		[ "$#" -gt 0 ] || set -- qbittorrent luci-app-qbittorrent luci-i18n-qbittorrent-zh-cn
+	    		apk add --no-cache --initdb --no-scripts --no-network --preserve-env \
+	    			--arch "${pkg_arch}" \
+	    			--repositories-file /tmp/.qbt/apk/repositories.d/customfeeds.list \
+	    			--repository ${script_dir}/pkgs/packages.adb $@
 
-    		mkdir -p /var/opkg-lists/
-    		cp ${script_dir}/pkgs/Packages.gz /var/opkg-lists/openwrt_qbt
-    		cp ${script_dir}/pkgs/Packages.sig /var/opkg-lists/openwrt_qbt.sig
+	    		rm -rf /etc/apk/keys/$fingerprint \
+	    			/tmp/.qbt/apk/repositories.d/customfeeds.list
+	    	;;
+	    	remove)
+	    		apk del --force-broken-world $@
+	    	;;
+	    	*)
+	    		echo "Usage:"
+	    		echo "	$0 [sub-command]"
+	    		echo ""
+	    		echo "Commands:"
+	    		echo "	install			Install qbittorrent and its depends"
+	    		echo "	remove <pkgs>		Uninstall pkgs"
+	    		echo ""
+	    	;;
+	    esac
+	EOF
+else
+	sed 's/^    //g' > ${SAVE_ROOT_DIR}/install.sh <<-"EOF"
+	    #!/bin/sh
+	    work_dir=$(pwd)
+	    script_dir="$(cd "$( dirname "$0" )" && pwd)"
 
-    		[ "$#" -gt 0 ] || set -- qbittorrent luci-app-qbittorrent luci-i18n-qbittorrent-zh-cn
-    		opkg install $@
-    		sed -i "/src\/gz openwrt_qbt file:\/\/$(echo ${script_dir}/pkgs | sed 's/\//\\\//g')/d" /etc/opkg/customfeeds.conf
-    		rm -rf /etc/opkg/keys/$fingerprint
-    	;;
-    	remove)
-    		opkg --force-removal-of-dependent-packages $@
-    	;;
-    	*)
-    		echo "Usage:"
-    		echo "	$0 [sub-command]"
-    		echo ""
-    		echo "Commands:"
-    		echo "	install			Install qbittorrent and its depends"
-    		echo "	remove <pkgs>		Uninstall pkgs"
-    		echo ""
-    	;;
-    esac
+	    cd ${work_dir}
 
-    [ "$add_arch" != 1 ] || sed -i '/# qbt add start/{:a;N;/# qbt add end/!ba;d}' /etc/opkg.conf
-EOF
+	    if [ "$(opkg print-architecture | sed -n 's/arch \(\S\+\) 10/\1/pg')" != "${PKG_ARCH}" ]; then
+	    	add_arch=1
+	    	cat >> /etc/opkg.conf <<-EOF1
+	    		# qbt add start
+	    		$(opkg print-architecture)
+	    		arch ${PKG_ARCH} 1
+	    		# qbt add end"
+	    	EOF1
+	    fi
 
-sed -i -e "s/\${target_arch}/${target_arch}/g" \
+	    case "$1" in
+	    	install)
+	    		shift
+	    		cp ${script_dir}/key/$fingerprint /etc/opkg/keys
+	    		sed -i "\$asrc\/gz openwrt_qbt file:\/\/$(echo ${script_dir}/pkgs | sed 's/\//\\\//g')" /etc/opkg/customfeeds.conf
+
+	    		mkdir -p /var/opkg-lists/
+	    		cp ${script_dir}/pkgs/Packages.gz /var/opkg-lists/openwrt_qbt
+	    		cp ${script_dir}/pkgs/Packages.sig /var/opkg-lists/openwrt_qbt.sig
+
+	    		[ "$#" -gt 0 ] || set -- qbittorrent luci-app-qbittorrent luci-i18n-qbittorrent-zh-cn
+	    		opkg install $@
+	    		sed -i "/src\/gz openwrt_qbt file:\/\/$(echo ${script_dir}/pkgs | sed 's/\//\\\//g')/d" /etc/opkg/customfeeds.conf
+	    		rm -rf /etc/opkg/keys/$fingerprint
+	    	;;
+	    	remove)
+	    		opkg --force-removal-of-dependent-packages $@
+	    	;;
+	    	*)
+	    		echo "Usage:"
+	    		echo "	$0 [sub-command]"
+	    		echo ""
+	    		echo "Commands:"
+	    		echo "	install			Install qbittorrent and its depends"
+	    		echo "	remove <pkgs>		Uninstall pkgs"
+	    		echo ""
+	    	;;
+	    esac
+
+	    [ "$add_arch" != 1 ] || sed -i '/# qbt add start/{:a;N;/# qbt add end/!ba;d}' /etc/opkg.conf
+	EOF
+fi
+
+sed -i -e "s/\${PKG_ARCH}/${PKG_ARCH}/g" \
 	-e "s/\$fingerprint/${fingerprint}/g" ${SAVE_ROOT_DIR}/install.sh
 
-SAVED_NAME="${target_arch}-${CUR_LINK_TYPE}-qt${CUR_QT_VERSION}-lt_${CUR_LIBT_VERSION}"
+SAVED_NAME="${PKG_ARCH}-${CUR_LINK_TYPE}-qt${CUR_QT_VERSION}-lt_${CUR_LIBT_VERSION}"
 # Add SAVED_NAME to the environment variables
 echo "SAVED_NAME=${SAVED_NAME}" >> $GITHUB_ENV
 
-[ ! -f ${PKGS_DIR}/qbittorrent*.ipk ] || {
+[ ! -f ${PKGS_DIR}/qbittorrent*.${PKG_EXT} ] || {
 	echo "pkgs=true" >> $GITHUB_OUTPUT
 	cd ..
 	ln -sf "${SAVE_ROOT_DIR}" "${SAVED_NAME}"
@@ -137,7 +201,7 @@ echo "SAVED_NAME=${SAVED_NAME}" >> $GITHUB_ENV
 }
 
 ## Compress and encrypt the keychain
-# tar -czvf - ${BUILD_KEY}.pub ${BUILD_KEY} | \
+# tar -czvf - ${BUILD_KEY_OPKG_NAME}.pub ${BUILD_KEY_OPKG_NAME} | \
 # openssl enc -aes-256-ctr -pbkdf2 -pass pass:${KEYCHAIN_SECRET} > ${SAVE_ROOT_DIR}-keychain.bin
 ## openssl enc -d -aes-256-ctr -pbkdf2 -pass pass:123456 -in ${SAVE_ROOT_DIR}-keychain.bin | tar -xz
 
