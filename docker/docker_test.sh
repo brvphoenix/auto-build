2f4836f87d74427f8473b99b96a12e557ebba7d4
brvphoenix <30111323+brvphoenix@users.noreply.github.com>
2026-06-25 23:34:43 +0800

    Optimize shutdown loop by removing duplicate request
    Removed redundant shutdown request in the loop.

--- docker/docker_test.sh
+++ docker/docker_test.sh
@@ -86,7 +86,6 @@ if [ -n "$(docker ps -f id=${docker_id} -f status=running -q)" ]; then
 		end_time=$(($(date +%s) + 300))
 		while [ "${end_time}" -gt "$(date +%s)" ]; do
 			[ -n "$(docker ps -f id=${docker_id} -f status=running -q)" ] && sleep 1 || { err_code=0; break; }
-			curl -s -m 10 -X POST -H "Host: ${webui_url}" --cookie "${sid_name}=${sid}" ${req_url}/api/v2/app/shutdown
 		done
 	else
 		echo "::error::Can't connect to qbittorrent!!!"
